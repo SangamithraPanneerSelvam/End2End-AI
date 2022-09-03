2fed92b49f19cdf316381c2f24f10d6cc4e1ddd3
unknown <selvam@ifb.uni-stuttgart.de>
2022-09-03 17:10:35 +0200

    Data Processing

---
+++ DataProcessing/DataProcessing.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+class DataProcessing:
+
+  def __init__(self):
+    self.df=pd.DataFrame()
+    
+    
+  def load(self):
+    dataset=pd.read_csv("wedding_info.csv")
+    return dataset
+   
+
+  def imputing(self,dataset,lis:list):
+    for i in lis:
+      self.df=dataset.replace(i,'',regex=True)
+
+    self.df=self.df.apply(lambda x:x.str.strip() if x.dtype == "object" else x)
+    self.__fillers() 
+  
+  def __fillers(self):
+    self.df.iloc[:,0:10]=self.df.iloc[:,0:10].fillna("None")
+    self.df.iloc[:,10:20]=self.df.iloc[:,10:20].fillna('$0')
+    self.__Estimate()
+
+  def __Estimate(self):
+    self.df['Estimate']=self.df['Estimate'].map(lambda x:x.lstrip('$').rstrip(' is the Estimated Wedding Cost')).astype(int)
+    self.__strInt()
+  
+  def __strInt(self):
+    for column in df.columns[11:20]:
+      self.df[column]=self.df[column].map(lambda x:x.lstrip('$'))
+      self.df[column]=self.df[column].astype(int)
+      self.__save()
+  
+  def __save(self):
+    self.df.to_csv('processed_data.csv')
+
+if __name__=="__main__":
+  obj=DataProcessing()
+  dataset= obj.load()
+  obj.imputing(dataset,['\n','\t',','])
+  
